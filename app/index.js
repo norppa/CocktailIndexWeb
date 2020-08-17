aea148b6b1ab62ca75e9b8a1d1241cdcbd914a39
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-17 18:06:27 +0300

    remove index.css

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 
 import Viewer from './components/viewer/Viewer'
 import Editor from './components/editor/Editor'
