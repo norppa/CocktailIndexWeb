e5c6194845e8d1d3514138787221f2735636bcec
Jari Haavisto <jari.haavisto@gmail.com>
2020-09-28 23:15:33 +0300

    add zombie and not selected images

--- app/img/images.js
+++ app/img/images.js
@@ -3,11 +3,15 @@ import coupe from './coupe.png'
 import oldFashioned from './old_fashioned.png'
 import doubleOldFashioned from './double_old_fashioned.png'
 import hurricane from './hurricane.png'
+import zombie from './zombie.png'
+import notSelected from './not_selected.png'
 
 export default {
     'Coupe': coupe,
     'Old Fashioned': oldFashioned,
     'Double Old Fashioned': doubleOldFashioned,
     'Hurricane': hurricane,
+    'Zombie': zombie,
+    'Not Selected': notSelected,
     'dot': dot
 }
